03cefd137dc380a4915de565312269ecf6bb2472
zhijie lee <onezeros.lee@gmail.com>
2012-08-25 11:02:17 +0800

    colorshift effect done

--- psx4html5/PsxEffects/PsxEffects.colorshift.js
+++ psx4html5/PsxEffects/PsxEffects.colorshift.js
@@ -21,3 +21,58 @@
  *
  */
 
+PsxEffects.ColorShift = {};
+
+// colorshift effect reserves given hue value in the picture and turn other pixels gray
+// paramters: hueCenter,the hue you want to reserve, value ranges in [0,360]; 
+//            radius, range of similar color that can be reserved
+PsxEffects.ColorShift.colorshift = function(src,_dst,hueCenter,radius)
+{
+    var dst = _dst;
+    if(dst == src){
+        dst = PSX.Util.cloneImageData(src);
+    }
+    var hsv = PSX.Util.cloneImageData(src);
+    PSX.Image.Mode.rgb2hsv(src,hsv);
+    
+    PSX.Image.Mode.rgb2gray(src,dst);
+    hueCenter /= 2;
+    radius /= 2;
+    var len = src.data.length;
+    for (var i=0; i < len; i+=4) {
+        var dist = Math.abs(hsv.data[i] - hueCenter);
+        if( dist < radius || dist > 180 - radius){
+            for(var ch =0; ch < 4; ch++){
+                dst.data[i + ch] = src.data[i + ch];
+            }
+        }
+    };
+    if(dst != _dst){
+        PSX.Util.copyArray(dst.data,_dst.data);
+    }
+}
+
+PsxEffects.ColorShift.color1 = function(src,dst)
+{
+    PsxEffects.ColorShift.colorshift(src,dst,36,36);
+}
+
+PsxEffects.ColorShift.color2 = function(src,dst)
+{
+    PsxEffects.ColorShift.colorshift(src,dst,108,36);
+}
+
+PsxEffects.ColorShift.color3 = function(src,dst)
+{
+    PsxEffects.ColorShift.colorshift(src,dst,180,36);
+}
+
+PsxEffects.ColorShift.color4 = function(src,dst)
+{
+    PsxEffects.ColorShift.colorshift(src,dst,252,36);
+}
+
+PsxEffects.ColorShift.color5 = function(src,dst)
+{
+    PsxEffects.ColorShift.colorshift(src,dst,324,36);
+}
